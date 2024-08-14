18f267841d8ea737e01fa6e41f0201335cb167aa
wangguangwu <wangguangwu@pukanghealth.com>
2024-08-14 16:06:43 +0800

    first commit

---
+++ src/main/java/com/wangguangwu/springbootevent/SpringBootEventApplication.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.springbootevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEventApplication.class, args);
+	}
+
+}
